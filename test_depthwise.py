96f34bf0a6c01abdb54a69f2d0702097012af578
Steve Burns <steven.m.burns@intel.com>
2021-08-30 16:45:21 -0700

    Create a stream of instructions

--- test_depthwise.py
+++ test_depthwise.py
@@ -1,5 +1,16 @@
 from itertools import product
 import numpy as np
+import pytest
+
+from functools import wraps
+
+def logging_decorator(fn):
+    @wraps(fn)
+    def func(*a, **kw):
+        print(f'{fn.__name__}({a}, {kw})')
+        return fn(*a, *kw)
+    return func
+
 
 BLOCK_IN = 16
 BLOCK_OUT = 4
@@ -160,57 +171,7 @@ def tiled_and_buffered(inp, wgt):
 
     return out
 
-
-def depthwise_conv(range0, range1,
-                   inp_stride0, inp_stride1,
-                   wgt_stride0, wgt_stride1,
-                   out_stride0, out_stride1,
-                   uop_codes):
-    """S (convolution stride) should be a parameter"""
-    def ibi(i, j, c, o):
-        return o * BLOCK_OUT + c + inp_stride1 * j + inp_stride0 * i
-
-    def wbi(k0, k1, c, o):
-        return o * BLOCK_IN * BLOCK_OUT + c + wgt_stride1 * k1 + wgt_stride0 * k0
-
-    def obi(i, j, c, o):
-        return o * BLOCK_OUT + c + out_stride1 * j + out_stride0 * i
-
-    for i_inner in range(range0):
-        for j_inner in range(range1):
-            for inp_off, wgt_off, out_off in uop_codes:
-                for c_inner in range(TC):
-                    inner = 0
-                    for k0, k1 in product(range(3), range(3)):
-                        inner += ex(wb[wbi(k0, k1, c_inner, wgt_off)]) * ex(ib[ibi(i_inner + k0, j_inner + k1, c_inner, inp_off)])
-                    if i_inner % S == 0 and j_inner % S == 0:
-                        ob[obi(i_inner // S, j_inner // S, c_inner, out_off)] = inner
-
-
-def depthwise_conv2(range0, range1,
-                    inp_stride0, inp_stride1,
-                    wgt_stride0, wgt_stride1,
-                    out_stride0, out_stride1,
-                    uop_codes):
-    """S (convolution stride) should be a parameter"""
-    def ibi(i, j, c, o):
-        return o * BLOCK_OUT + c + inp_stride1 * j + inp_stride0 * i
-
-    def wbi(k0, k1, c, o):
-        return o * BLOCK_IN * BLOCK_OUT + c + wgt_stride1 * k1 + wgt_stride0 * k0
-
-    def obi(i, j, c, o):
-        return o * BLOCK_OUT + c + out_stride1 * j + out_stride0 * i
-
-    for i_inner in range(range0):
-        for j_inner in range(range1):
-            for inp_off, wgt_off, out_off in uop_codes:
-                for c_inner in range(TC):
-                    inner = 0
-                    for k0, k1 in product(range(3), range(3)):
-                        inner += ex(wb[wbi(k0, k1, c_inner, wgt_off)]) * ex(ib[ibi(i_inner + k0, j_inner + k1, c_inner, inp_off)])
-                    if i_inner % S == 0 and j_inner % S == 0:
-                        ob[obi(i_inner // S, j_inner // S, c_inner, out_off)] = inner
+@logging_decorator
 def depthwise_conv1(range0, range1,
                     inp_stride0, inp_stride1,
                     wgt_stride0, wgt_stride1,
@@ -288,6 +249,7 @@ def test_linebuffer():
     assert count == (lb.width-2)**2
 
 
+@logging_decorator
 def depthwise_conv2(range0, range1,
                     inp_stride0, inp_stride1,
                     wgt_stride0, wgt_stride1,
@@ -326,11 +288,13 @@ def depthwise_conv2(range0, range1,
                         sq[k0,k1,c_inner] = ib[ibi(i_inner + k0, j_inner + k1, c_inner, inp_off)]
                 sink.send( (sq, i_inner, j_inner, wgt_off, out_off))
 
+@logging_decorator
 def depthwise_conv3(range0, range1,
                     inp_stride0, inp_stride1,
                     wgt_stride0, wgt_stride1,
                     out_stride0, out_stride1,
                     uop_codes):
+
     """S (convolution stride) should be a parameter"""
     def ibi(i, j, c, o):
         return o * BLOCK_OUT + c + inp_stride1 * j + inp_stride0 * i
@@ -367,11 +331,13 @@ def depthwise_conv3(range0, range1,
                     sink.send( (sq, i_inner-2, j_inner-2, wgt_off, out_off))
 
 
+@logging_decorator
 def load_wgt_inst( c_off, c_max, wgt, buf_off):
     for c_inner in range(c_max):
         for k0, k1 in product(range(3), range(3)):
             wb[buf_off + wbi(k0, k1, c_inner)] = wgt[wi2(k0, k1, c_off + c_inner)]
 
+@logging_decorator
 def load_inp_inst( i_off, j_off, c_off, i_max, j_max, c_max, inp, buf_off):
     for i_inner in range(i_max):
         for j_inner in range(j_max):
@@ -379,6 +345,7 @@ def load_inp_inst( i_off, j_off, c_off, i_max, j_max, c_max, inp, buf_off):
                 ib[buf_off + ibi(i_inner, j_inner, c_inner)] = \
                     inp[ii2(i_off + i_inner, j_off + j_inner, c_off + c_inner)]
 
+@logging_decorator
 def store_inst( i_off, j_off, c_off, i_max, j_max, c_max, out, buf_off):
     for i_inner in range(i_max):
         for j_inner in range(j_max):
@@ -386,6 +353,27 @@ def store_inst( i_off, j_off, c_off, i_max, j_max, c_max, out, buf_off):
                 out[oi2(i_off + i_inner, j_off + j_inner, c_off + c_inner)] = \
                     ob[buf_off + obi(i_inner, j_inner, c_inner)]
     
+
+def call_instruction( inp, wgt, out, depthwise_inst, tbl):
+    if tbl['nm'] == 'depthwise_inst':
+        depthwise_inst(
+            tbl['range0'], tbl['range1'],
+            tbl['inp_stride0'], tbl['inp_stride1'],
+            tbl['wgt_stride0'], tbl['wgt_stride1'],
+            tbl['out_stride0'], tbl['out_stride1'],
+            tbl['uop_codes'])
+    elif tbl['nm'] == 'load_wgt_inst':
+        load_wgt_inst( tbl['c_off'], tbl['c_max'], wgt, tbl['buf_off']) 
+    elif tbl['nm'] == 'load_inp_inst':
+        load_inp_inst( tbl['i_off'], tbl['j_off'], tbl['c_off'],
+                       tbl['i_max'], tbl['j_max'], tbl['c_max'],
+                       inp, tbl['buf_off'])
+    elif tbl['nm'] == 'store_inst':
+        store_inst( tbl['i_off'], tbl['j_off'], tbl['c_off'],
+                       tbl['i_max'], tbl['j_max'], tbl['c_max'],
+                       out, tbl['buf_off'])
+    else:
+        assert False
 
 def tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv3):
     assert C % TC == 0
@@ -396,26 +384,37 @@ def tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv3):
     wgt_parity = 0
     inp_parity = 0
 
+    def ci( tbl):
+        call_instruction( inp, wgt, out, depthwise_inst, tbl)
+
     for c_outer in range(C // TC):
-        load_wgt_inst( c_outer*TC, TC, wgt, wgt_parity*wb_sz)
+        ci( {'nm': 'load_wgt_inst', 'c_off': c_outer*TC, 'c_max': TC, 'buf_off': wgt_parity*wb_sz})
 
         for i_outer in range((H + TH - 1) // TH):
             for j_outer in range((W + TW - 1) // TW):
-                load_inp_inst( i_outer*TH, j_outer*TW, c_outer*TC,
-                               min(H - i_outer * TH, TH) + 2,
-                               min(W - j_outer * TW, TW) + 2,
-                               TC,
-                               inp, inp_parity*ib_sz)
+                ci({'nm': 'load_inp_inst', 'i_off': i_outer*TH, 'j_off': j_outer*TW, 'c_off': c_outer*TC,
+                    'i_max': min(H - i_outer * TH, TH) + 2,
+                    'j_max': min(W - j_outer * TW, TW) + 2,
+                    'c_max': TC,
+                    'buf_off': inp_parity*ib_sz})
 
-                depthwise_inst(min(H - i_outer * TH, TH), min(W - j_outer * TW, TW),
-                               TC * (TW + 2), TC, TC * 3, TC, TW // S * TC, TC,
-                               [(inp_parity*ib_sz//ib_lsz, wgt_parity*wb_sz//wb_lsz, inp_parity*ob_sz//ob_lsz)])
+                ci({'nm': 'depthwise_inst',
+                    'range0': min(H - i_outer * TH, TH),
+                    'range1': min(W - j_outer * TW, TW),
+                    'inp_stride0': TC * (TW + 2), 'inp_stride1': TC,
+                    'wgt_stride0': TC * 3, 'wgt_stride1': TC,
+                    'out_stride0': TW // S * TC, 'out_stride1': TC,
+                    'uop_codes': [(inp_parity*ib_sz//ib_lsz,
+                                   wgt_parity*wb_sz//wb_lsz,
+                                   inp_parity*ob_sz//ob_lsz)]
+                    }
+                   )
 
-                store_inst(i_outer*TH//S, j_outer*TW//S, c_outer*TC,
-                           (min(H - i_outer * TH, TH) + S - 1) // S,
-                           (min(W - j_outer * TW, TW) + S - 1) // S,
-                           TC,
-                           out, inp_parity*ob_sz)
+                ci({'nm': 'store_inst', 'i_off': i_outer*TH//S, 'j_off': j_outer*TW//S, 'c_off': c_outer*TC,
+                    'i_max': (min(H - i_outer * TH, TH) + S - 1) // S,
+                    'j_max': (min(W - j_outer * TW, TW) + S - 1) // S,
+                    'c_max': TC,
+                    'buf_off': inp_parity*ob_sz})
 
                 inp_parity = (inp_parity+1) % 2
 
@@ -423,44 +422,34 @@ def tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv3):
     return out
 
 
-def test_A():
-    wgt = np.random.randint(0, 127, size=(wgt_sz(),), dtype=np.int8)
-    inp = np.random.randint(0, 127, size=(inp_sz(),), dtype=np.int8)
-
+@pytest.fixture
+def random_test():
+    wgt = np.random.randint(-128, 128, size=(wgt_sz(),), dtype=np.int8)
+    inp = np.random.randint(-128, 128, size=(inp_sz(),), dtype=np.int8)
     out_gold = gold(inp, wgt)
+    return wgt,inp,out_gold
+
+def test_A(random_test):
+    wgt,inp,out_gold = random_test
     out = tiled(inp, wgt)
     assert (out_gold == out).all()
 
-
-def test_B():
-    wgt = np.random.randint(0, 127, size=(wgt_sz(),), dtype=np.int8)
-    inp = np.random.randint(0, 127, size=(inp_sz(),), dtype=np.int8)
-
-    out_gold = gold(inp, wgt)
+def test_B(random_test):
+    wgt,inp,out_gold = random_test
     out = tiled_and_buffered(inp, wgt)
     assert (out_gold == out).all()
 
-
-def test_C1():
-    wgt = np.random.randint(0, 127, size=(wgt_sz(),), dtype=np.int8)
-    inp = np.random.randint(0, 127, size=(inp_sz(),), dtype=np.int8)
-
-    out_gold = gold(inp, wgt)
+def test_C1(random_test):
+    wgt,inp,out_gold = random_test
     out = tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv1)
     assert (out_gold == out).all()
 
-def test_C2():
-    wgt = np.random.randint(0, 127, size=(wgt_sz(),), dtype=np.int8)
-    inp = np.random.randint(0, 127, size=(inp_sz(),), dtype=np.int8)
-
-    out_gold = gold(inp, wgt)
+def test_C2(random_test):
+    wgt,inp,out_gold = random_test
     out = tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv2)
     assert (out_gold == out).all()
 
-def test_C3():
-    wgt = np.random.randint(0, 127, size=(wgt_sz(),), dtype=np.int8)
-    inp = np.random.randint(0, 127, size=(inp_sz(),), dtype=np.int8)
-
-    out_gold = gold(inp, wgt)
+def test_C3(random_test):
+    wgt,inp,out_gold = random_test
     out = tiled_and_buffered_mapped(inp, wgt, depthwise_inst=depthwise_conv3)
     assert (out_gold == out).all()
